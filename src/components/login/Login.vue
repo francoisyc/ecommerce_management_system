a4995f9c025a9103dfca30a0563011e540b4ccb4
unknown <409704591@qq.com>
2020-06-08 16:26:00 +0800

    02-登录功能基本完成

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -1,67 +1,96 @@
 <template>
-  <div>
-    <el-form
-      :model="ruleForm"
-      :rules="rules"
-      ref="ruleForm"
-      label-width="100px"
-      class="demo-ruleForm"
-      label-position='top'
-    >
+  <div class="login-wrapper">
+    <el-row type="flex" class="loginForm" justify="center" align="middle">
+      <el-col :xs="12" :sm="10" :md="8" :lg="6" :xl="4" class="login-content">
+       <el-form :model="loginForm" :rules="rules"  ref="loginForm" label-width="100px"  label-position="top">
       <el-form-item label="用户名" prop="username">
-        <el-input v-model="ruleForm.username"></el-input>
+        <el-input v-model="loginForm.username"></el-input>
       </el-form-item>
       <el-form-item label="密码" prop="password">
-        <el-input v-model="ruleForm.password"></el-input>
+        <el-input v-model="loginForm.password" show-password></el-input>
       </el-form-item>
-      
-      <el-form-item >
-        <el-button type="primary" @click="submitForm('ruleForm')">登录</el-button>
-        <el-button @click="resetForm('ruleForm')">重置</el-button>
+
+      <el-form-item>
+        <el-button type="primary" @click="submitForm">登录</el-button>
+        <el-button @click="resetForm('loginForm')">重置</el-button>
       </el-form-item>
     </el-form>
+      </el-col>
+    </el-row>
+    
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   data() {
     return {
-      ruleForm: {
+      loginForm: {
         username: "",
-        password: "",
-        
+        password: ""
       },
       rules: {
         username: [
           { required: true, message: "请输入用户名", trigger: "blur" },
           { min: 3, max: 15, message: "长度在 3 到 15 个字符", trigger: "blur" }
         ],
-         password: [
+        password: [
           { required: true, message: "请输入密码", trigger: "blur" },
           { min: 3, max: 15, message: "长度在 3 到 15 个字符", trigger: "blur" }
-        ],
-    
+        ]
       }
     };
   },
   methods: {
-    submitForm(formName) {
-      this.$refs[formName].validate(valid => {
+    //登录功能
+    login() {
+      axios
+        .post("http://localhost:8888/api/private/v1/login", this.loginForm)
+        .then(res => {
+          const { data, meta } = res.data;
+          console.log(data, meta);
+          if (meta.status === 200) {
+            alert("登录成功");
+            this.$router.push("/home");
+          } else {
+            this.$message({
+              type: "error",
+              message: meta.msg,
+              duration: 1000
+            });
+          }
+        });
+    },
+    submitForm() {
+      this.$refs.loginForm.validate(valid => {
         if (valid) {
-          alert("submit!");
+          this.login();
         } else {
           console.log("error submit!!");
           return false;
         }
       });
     },
-    resetForm(formName) {
-      this.$refs[formName].resetFields();
+    resetForm() {
+      this.$refs.loginForm.resetFields();
     }
   }
 };
 </script>
 
 <style>
+.login-wrapper,
+.loginForm{
+    height: 100%;
+}
+.loginForm{
+    background-color:#2D434C;
+}
+.login-content{
+    min-width: 240px;
+    background-color: #fff;
+    padding: 20px 35px;
+    border-radius: 10px;
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/login/Login'
+import Home from '@/components/home/Home'
 
 
 Vue.use(Router)
@@ -11,6 +12,10 @@ export default new Router({
       path: '/login',
       component:Login
      
+    },
+    {
+      path:'/home',
+      component:Home
     }
   ]
 })
